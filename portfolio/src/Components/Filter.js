6107b3e24492cb9d0c7dbfac92c35084a0e6acac
bobkong <bobkong96@outlook.com>
2022-10-05 00:37:33 -0700

    fix filter bug

--- portfolio/src/Components/Filter.js
+++ portfolio/src/Components/Filter.js
@@ -10,7 +10,7 @@ function Filter() {
     <div id="filter-div">
         <li onClick={() => {setTag(0)}} className={tag == 0 ? "selectedTag" : "unSelectedTag"}>Product Design</li>
         <li onClick={() => {setTag(1)}} className={tag == 1 ? "selectedTag" : "unSelectedTag"}>Development</li>
-        <li onClick={() => {setTag(2)}} className={tag == 3 ? "selectedTag" : "unSelectedTag"}>Fun Work</li>
+        <li onClick={() => {setTag(2)}} className={tag == 2 ? "selectedTag" : "unSelectedTag"}>Fun Work</li>
     </div>);
 }
 
