976d7a5fc00717c59d9abedb0fc4face8785d004
bobkong <bobkong96@outlook.com>
2022-10-05 14:40:10 -0700

    change design filter

--- portfolio/src/Components/Filter.js
+++ portfolio/src/Components/Filter.js
@@ -8,7 +8,7 @@ function Filter() {
 
     return (
     <div id="filter-div">
-        <li onClick={() => {setTag(0)}} className={tag == 0 ? "selectedTag" : "unSelectedTag"} style={{ cursor: "pointer" }}>Product Design</li>
+        <li onClick={() => {setTag(0)}} className={tag == 0 ? "selectedTag" : "unSelectedTag"} style={{ cursor: "pointer" }}>Design</li>
         <li onClick={() => {setTag(1)}} className={tag == 1 ? "selectedTag" : "unSelectedTag"} style={{ cursor: "pointer" }}>Development</li>
         <li onClick={() => {setTag(2)}} className={tag == 2 ? "selectedTag" : "unSelectedTag"} style={{ cursor: "pointer" }}>Fun Work</li>
     </div>);
