b18810460a0eed6e843fafecba791162e65f4225
bobkong <bobkong96@outlook.com>
2022-10-10 13:40:52 -0700

    update personal website content

--- portfolio/src/Components/DevelopWork.js
+++ portfolio/src/Components/DevelopWork.js
@@ -5,6 +5,7 @@ import ReactPlayer from 'react-player';
 import QQMusicFeedback from '../Assets/qqmusic-feedback.png';
 import QQMusicTeam1 from '../Assets/qqmusic-team-1.jpeg';
 import QQMusicTeam2 from '../Assets/qqmusic-team-2.JPG';
+import Certificate from '../Assets/certificate.png';
 
 
 const development = [
@@ -19,44 +20,23 @@ const development = [
     takeaway: '1. The work at Tencent made me aware of the power of design. Currently, I value friendly user experience and design in my daily work and I am also determined to design and ship beautiful designs to the whole world. \n2. Through more than two years of agile development, I have greatly improved my software development capabilities. I have improved my ability to implement the user interface, I can develop and deliver the design draft with high quality very quickly and confidently.'},
 
     // Instagram
-    {projectName: "Instagram AR", 
-    video: "", 
-    language: "AR Prototyping: Reactive, JavaScript", 
-    type: "Internship", 
-    about: "New Instagram AR effects utilizing the hand-tracking capability.", 
-    github: '',
-    timeline: "June 2022 - September 2022",
-    takeaway: ""},
+    {projectName: "Instagram AR"},
 
     // Personal Website
     {projectName: "Personal Website", 
     video: "", 
     language: "Front-end: React, HTML, CSS, Javascript", 
     type: "Personal Project", 
-    about: "A portfolio built during Meta Front-end Development Certificate courses.", 
+    about: "As an Android engineer who has learned Android development for more than 5 years and worked as a full-time Android developer for more than 2 years, when I saw the job requirement for UX Engineer/Design technologist, I felt confused about why HTML/CSS/JS/React is required and why XML/Java/Kotlin is generally not required. \nTo figure out this question, I enrolled in Meta Front-end Development Professional Certificate, and I know that front-end development is more versatile! Android applications can only run on Android devices but web applications can not only run on the web browser but also on mobile devices. Thousands of apps are conducted with React Native, including Facebook, Microsoft Office, Discord, and more! Also, with web responsive development support, developers can make their web applications look perfect on all-size devices!! So it's not hard to understand why HTML/CSS/JS/React is required for UX engineers and Design Technologists!! \nSo far I've achieved 4 certificates: Introduction to front-end development, Programming with Javascript, HTML, and CSS in depth, and React Basics. And to practice what I learned in this course and learn more front-end development skills, I also developed this website with React from 0 to 1 and deployed it to GitHub. ", 
     github: 'https://github.com/Bobkong/portfolio/tree/main/portfolio',
     timeline: "September 2022 - October 2022",
     takeaway: ""},
 
     // EasyMed
-    {projectName: "EasyMed", 
-    video: "", 
-    language: "Android: Java, Kotlin, XML, JSON", 
-    type: "Microsoft AI Hackathon", 
-    about: "An AI-assisted application for checking drug and food interactions.", 
-    github: 'https://github.com/Bobkong/Easy-Drug-Android',
-    timeline: "May 2022 - June 2022",
-    takeaway: ""},
+    {projectName: "EasyMed"},
 
     // ARamsay
-    {projectName: "ARamsay", 
-    video: "", 
-    language: "AR & Back-end: Unity3D, C#. Nodejs", 
-    type: "Course Project", 
-    about: "An AR cooking assistant on HoloLens 2 for learning recipes more smoothly.", 
-    github: 'https://github.com/ARamsay118',
-    timeline: "November 2021 - December 2021",
-    takeaway: ""},
+    {projectName: "ARamsay"},
 
     // Super Plant
     {projectName: "Super Plant", 
@@ -97,6 +77,16 @@ function GithubRepo(props) {
     }
 }
 
+function VideoPlayer(props) {
+    if (props.video != null && props.video != "") {
+        return (
+        <div>
+            <p className='title-color title-size'>Video</p>
+            <ReactPlayer className='react-player' url={props.video} controls={true} width='100%'/>
+        </div>);
+    }
+}
+
 function QQMusic(props) {
     if (props.projectName != null && props.projectName == "Tencent Music") {
         return (
@@ -108,6 +98,28 @@ function QQMusic(props) {
                     <img id="qqmusic-team-img" src={QQMusicTeam1} />
                     <img id="qqmusic-team-img" src={QQMusicTeam2} />
                 </div>
+                
+            </div>
+            
+        );
+    }
+}
+
+function PersonalWebsite(props) {
+    if (props.projectName != null && props.projectName == "Personal Website") {
+        return (
+            <div>
+                <img id="qqmusic-feedback" src={Certificate} />
+            </div>
+            
+        );
+    }
+}
+
+function Takeaway(props) {
+    if (props.takeaway != null && props.takeaway != "") {
+        return (
+            <div>
                 <p className='title-color title-size'>Takeaway</p>
                 <p className='body-color body-size' style={{whiteSpace: 'pre-wrap', lineHeight: 1.5}}>{props.takeaway}</p>
             </div>
@@ -130,9 +142,10 @@ function DevelopWork(props) {
         <hr id='divide-line'/>
         <p className='title-color title-size'>About</p>
         <p className='body-color body-size' style={{whiteSpace: 'pre-wrap', lineHeight: 1.5}}>{project.about}</p>
-        <p className='title-color title-size'>Video</p>
-        <ReactPlayer className='react-player' url={project.video} controls={true} width='100%'/>
+        <PersonalWebsite projectName={project.projectName} />
+        <VideoPlayer video={project.video}/>
         <QQMusic projectName={project.projectName} />
+        <Takeaway takeaway={project.takeaway} />
     </div>);
 }
 
