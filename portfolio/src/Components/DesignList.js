7f4ad53eadd210d71ca3b0d787734b070732faa0
bobkong <bobkong96@outlook.com>
2022-11-13 14:55:30 -0800

    homni health

--- portfolio/src/Components/DesignList.js
+++ portfolio/src/Components/DesignList.js
@@ -3,10 +3,10 @@ import FullCoverProject from './FullCoverProject';
 
 function DesignList() {
     const design = [{bg: require('../Assets/instagram_bg.png'), role: "AR DESIGN, UI/UX DESIGN", projectName: "Instagram AR", type: "Internship", description: "New Instagram AR effects utilizing the hand-tracking capability.", theme: 0, url: 'https://www.lingshuangkong.com/work/instagram'}, 
-    {bg: require('../Assets/easymed_bg.png'), role: "UX/UI DESIGN, ANDROID DEVELOPMENT", projectName: "EasyMed", type: "Microsoft AI Hackathon", description: "An AI-assisted application for checking drug and food interactions.", theme: 1, url: 'https://www.lingshuangkong.com/work/easymed'},
-    {bg: require('../Assets/homni_bg.png'), role: "UX/UI DESIGN", projectName: "Homni Stroke Detector", type: "Internship", description: "A machine learning-driven application for detecting patients' strokes.", theme: 1, url: 'https://www.lingshuangkong.com/work/homni-health'},
-    {bg: require('../Assets/nutrition_bg.png'), role: "UX/UI DESIGN", projectName: "Chipotle Nutrition", type: "Google Design Certificates", description: "A feature of Chipotle for finding out exactly how their meals stack up.", theme: 1, url: 'https://www.lingshuangkong.com/work/chipotle-nutrition-calculator'},
-    {bg: require('../Assets/aramsay_bg.gif'), role: "AR DESIGN, AR PROTOTYPING", projectName: "ARamsay", type: "Course Project", description: "An AR cooking assistant on HoloLens 2 for learning recipes more smoothly.", theme: 0, url: 'https://www.lingshuangkong.com/work/aramsay'},];
+    {bg: require('../Assets/homni_bg.png'), role: "UX/UI DESIGN", projectName: "Homni Stroke Detector", type: "Part-time", description: "A machine learning-driven application for detecting patients' strokes.", theme: 1, url: 'https://lingshuang.webflow.io/work/homni-health'},
+    {bg: require('../Assets/easymed_bg.png'), role: "UX/UI DESIGN, ANDROID DEVELOPMENT", projectName: "EasyMed", type: "Microsoft AI Hackathon", description: "An AI-assisted application for checking drug and food interactions.", theme: 1, url: 'https://lingshuang.webflow.io/work/easymed'},
+    {bg: require('../Assets/nutrition_bg.png'), role: "UX/UI DESIGN", projectName: "Chipotle Nutrition", type: "Google Design Certificates", description: "A feature of Chipotle for finding out exactly how their meals stack up.", theme: 1, url: 'https://lingshuang.webflow.io/work/chipotle-nutrition-calculator'},
+    {bg: require('../Assets/aramsay_bg.gif'), role: "AR DESIGN, AR PROTOTYPING", projectName: "ARamsay", type: "Course Project", description: "An AR cooking assistant on HoloLens 2 for learning recipes more smoothly.", theme: 0, url: 'https://lingshuang.webflow.io/work/aramsay'},];
 
     return (
     <div id="project-div-full">
